d0d66429bf9725bdd224873e647a2c7470106466
Niels Uchpa Salvador <niels_us@outlook.com>
2022-08-04 23:13:56 -0500

    Deploy Heroku

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { ConfigModule } from '@nestjs/config';
-import { EnvConfiguration } from './config/Env.config';
+import { EnvConfiguration } from './config/env.config';
 import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
